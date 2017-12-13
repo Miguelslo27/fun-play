0321405fb71be7285e89774534c2a8ebfdc5a318
GLOBANT\miguel.sosa <miguel.sosa@globant.com>
2017-12-13 14:45:29 -0300

    feature/title-bar-layout - Added title bar
    * Added layout and styles
    * Implemented the most basic logic

--- src/app/components/title-bar/title-bar.component.ts
+++ src/app/components/title-bar/title-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'title-bar',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title-bar.component.scss']
 })
 export class TitleBarComponent implements OnInit {
+  @Input('stream') stream: any = {}
 
   constructor() { }
 
