5d5f7e72d498fd70bb2a967acc6a41b896bdb3a5
Miguelslo27 <miguel.sosa@globant.com>
2017-12-14 11:50:24 -0300

    feature/play-list-layout - Added layout and pixel perfect styles

--- src/app/components/play-list/play-list.component.ts
+++ src/app/components/play-list/play-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'play-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play-list.component.scss']
 })
 export class PlayListComponent implements OnInit {
+
+  @Input('streamlist') streamList: any = [];
 
   constructor() { }
 
